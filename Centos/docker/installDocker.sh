8a41a63229c9c58bb6dfe7cbbfb7699915145658
numbnet <zusyurec@gmail.com>
2021-05-20 16:00:19 +0300

    Update installDocker.sh

--- Centos/docker/installDocker.sh
+++ Centos/docker/installDocker.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 
 # installDocker.sh
-
-echo "#     Установка Docker в Centos 7     #"
+wait()
+{
+  echo ""
+  echo "Нажмите любую клавишу, чтобы продолжить..."
+  read -s -n 1
+}
 
 ##********************************************************************
-#Установим репозитории EPEL и REMI
+echo "# --------- Установка Docker в Centos 7 --------- #"
+wait
 
+# Установим репозитории EPEL и REMI
 yum -y install epel-release
 yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-#Установим программное обеспечение и обновимся
+# программное обеспечение и обновимся
 yum -y install wget nano mc zip upzip htop yum-utils
 yum -y update
 
-# Установим репозиторий Docker
+# репозиторий Docker
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-###Устанавливаем Docker####
+# Уст. Docker####
 yum -y install docker-ce
 
-#Запускаем его и добавляем в автозагрузку
+# Запуск и доб. в автозагрузку
 systemctl start docker
 systemctl enable docker
 systemctl status docker
 
-#Для проверки, запустим образ hello-world
+# Для проверки, запустим образ hello-world
 docker run hello-world
-sleep 9
+
+echo " Уст. закончена Выходими. "
+wait
 exit
 ##********************************************************************
